e7a1c9623ec493eb9ad1e65304393a6a4c6d3929
Mark Andrey Acebu <markandrey.acebu@gmail.com>
2024-02-20 15:33:19 +0800

    Initial commit

---
+++ src/main/kotlin/com/practicetwo/numberapi/NumberApiApplication.kt
@@ -0,0 +1,11 @@
+package com.practicetwo.numberapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NumberApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<NumberApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/practicetwo/numberapi/NumberApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.practicetwo.numberapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NumberApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
